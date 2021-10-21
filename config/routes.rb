a6057aac8de72bea845c6b9849b9e94a40111efa
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-20 22:07:51 -0300

    Mapped controller for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
-  resources :posts
-  root "static_pages#landing_page"
-  # get 'static_pages/landing_page'
-  # get 'static_pages/privacy_policy'
-  get "privacy_policy", to: "static_pages#privacy_policy"
+  root 'home#index'
+  # get 'home/index'
+  # resources :posts
+  # root "static_pages#landing_page"
+  # # get 'static_pages/landing_page'
+  # # get 'static_pages/privacy_policy'
+  # get "privacy_policy", to: "static_pages#privacy_policy"
 end
