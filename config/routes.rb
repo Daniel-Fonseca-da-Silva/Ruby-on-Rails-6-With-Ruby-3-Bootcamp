baff7725591431bb833d9dab81650342fa547fa3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-20 22:20:08 -0300

    Refactored all html to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
-  # get 'home/index'
-  # resources :posts
-  # root "static_pages#landing_page"
-  # # get 'static_pages/landing_page'
-  # # get 'static_pages/privacy_policy'
-  # get "privacy_policy", to: "static_pages#privacy_policy"
 end
